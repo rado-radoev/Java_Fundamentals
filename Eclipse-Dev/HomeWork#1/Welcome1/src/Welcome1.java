52f0fcc6f1f941e3dd8e5b37c27bcf3794452251
superklamer <rradoev@gmail.com>
2017-04-05 21:27:21 -0700

    Homework #1

---
+++ Eclipse-Dev/HomeWork#1/Welcome1/src/Welcome1.java
@@ -0,0 +1,8 @@
+// Text printing method
+public class Welcome1 {
+	
+	// main method begins execution of Java application
+	public static void main(String[] args) {
+		System.out.println("Welcome to Java Programming!");
+	} // main method end
+} // end class Welcome1
