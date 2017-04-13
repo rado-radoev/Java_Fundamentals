ee81a5dc9b3b0b63d10347afcd81895182d6a520
superklamer <rradoev@gmail.com>
2017-04-12 22:41:50 -0700

    commission calculator

--- Eclipse-Dev/CommisionCalculator/src/CommissionCalculator.java
+++ Eclipse-Dev/CommisionCalculator/src/CommissionCalculator.java
@@ -14,19 +14,23 @@ public class CommissionCalculator {
     public static void main(String args[]) {
         // Initialize a Scanner to read input from the command line
     	Scanner scanner = new Scanner(System.in);
+    	SalesPerson salesPerson = new SalesPerson(); 
         int itemsSoldTotal = 0;
-        SalesPerson salesPerson = new SalesPerson(); 	
+        double totalEarnings = salesPerson.getSalary();
+        double commission = itemsSoldTotal * salesPerson.getCommission();
+    	int item = -1;
     	
-    	System.out.println("Item\tValue");
-        System.out.println("1\t$239.99");
-        System.out.println("2\t$129.75");
-        System.out.println("3\t$99.95");
-        System.out.println("4\t$350.89"); 
-        System.out.printf("Current compensation: %.2f%n", salesPerson.getSalary());
-    	System.out.println("Please select an item from the list above (or enter 0 to exit):");
-    	int item = scanner.nextInt();
-    	
-    	while (item != 0) {      	
+    	while (item != 0) {
+        	System.out.println("Item\tValue");
+            System.out.println("1\t$239.99");
+            System.out.println("2\t$129.75");
+            System.out.println("3\t$99.95");
+            System.out.println("4\t$350.89"); 
+            
+            System.out.printf("Current compensation: %.2f%n", totalEarnings);
+        	System.out.println("Please select an item from the list above (or enter 0 to exit):");
+        	item = scanner.nextInt();
+    		
     		switch (item) {
 			case 1:
 				itemsSoldTotal += 239.99;
@@ -45,41 +49,10 @@ public class CommissionCalculator {
 				break;
 			}
 
-        	double commission = itemsSoldTotal * salesPerson.getCommission();
-        	double totalEarnings = commission + salesPerson.getSalary();
-        	
-        	System.out.println("Item\tValue");
-            System.out.println("1\t$239.99");
-            System.out.println("2\t$129.75");
-            System.out.println("3\t$99.95");
-            System.out.println("4\t$350.89"); 
-            
-            System.out.printf("Current compensation: %.2f%n", totalEarnings);
-        	System.out.println("Please select an item from the list above (or enter 0 to exit):");
-        	item = scanner.nextInt();
-    		
-
-    	}
-    	
-    	double commission = itemsSoldTotal * salesPerson.getCommission();
-    	double totalEarnings = commission + salesPerson.getSalary();
-    	
+        	commission = itemsSoldTotal * salesPerson.getCommission();
+        	totalEarnings = commission + salesPerson.getSalary();
+    	} 	
     	System.out.printf("Total earnings: %.2f",totalEarnings);
-
-    	/* Display the user's current compensation
-    	 * 
-    	 * Prompt and take input from the user (you may assume that they will only enter int values)
-    	 * They'll enter an item number (1 - 4) to record its sale or 0 to exit
-    	 * 
-    	 * NOTE: THE USER DOES NOT ENTER PRICES DIRECTLY... THEY ENTER ITEM NUMBERS TO INDICATE WHAT WAS SOLD
-    	 * NOTE: THE USER MAY ENTER THE SAME ITEM NUMBRER MULTIPLE TIMES
-    	 * 
-    	 * If the user provides invalid input (a value other than 0 - 4) display "ERROR: Invalid input!" and prompt them again
-    	 */
-    	
-    	/* After the user enters 0, display the salesperson's earnings in the format "Total earnings: $NNN.NN" and exit
-    	 * For example, if the salesperson sold two item 3s this week the final output would be "Total earnings: $217.99"
-    	 */
     }
     
 }
