a8c95ccabd80d82147a7146ece0feb37bf9c35b9
Radoslav Radoev <rradoev@gmail.com>
2017-04-14 10:38:18 -0700

    Commission calculator - done

--- Eclipse-Dev/CommisionCalculator/src/CommissionCalculator.java
+++ Eclipse-Dev/CommisionCalculator/src/CommissionCalculator.java
@@ -6,7 +6,6 @@
  *
  * @author Radoslav Radoev
  */
-
 import java.util.Scanner;
 
 public class CommissionCalculator {
@@ -18,14 +17,20 @@ public class CommissionCalculator {
     	int item = -1;		// Initialize item variable that holds the user input
     	double commission;		// Total commission the sales person has earned
         double totalEarnings = salesPerson.getSalary();		// Total earning the sales person has made (salary + commission)
-          	
+        
+        // Create 4 item objects with prices
+        Item item1 = new Item(239.99);
+        Item item2 = new Item(129.75);
+        Item item3 = new Item(99.95);
+        Item item4 = new Item(350.89);
+          
         // Keep asking the user for input until 0 is entered
     	while (item != 0) {
         	System.out.println("Item\tValue");
-            System.out.println("1\t$239.99");
-            System.out.println("2\t$129.75");
-            System.out.println("3\t$99.95");
-            System.out.println("4\t$350.89"); 
+            System.out.printf("1\t$%.2f%n",item1.getPrice());
+            System.out.printf("2\t$%.2f%n",item2.getPrice());
+            System.out.printf("3\t$%.2f%n",item3.getPrice());
+            System.out.printf("4\t$%.2f%n",item4.getPrice());
             
             System.out.printf("Current compensation: %.2f%n", totalEarnings);
         	System.out.println("Please select an item from the list above (or enter 0 to exit):");
@@ -33,33 +38,32 @@ public class CommissionCalculator {
     		
         	// Add item's price to total items sold
     		switch (item) {
+    		case 0:
+    			break;
 			case 1:
-				itemsSoldTotal += 239.99;
+				itemsSoldTotal += item1.getPrice();
 				break;
 			case 2:
-				itemsSoldTotal += 129.75;
+				itemsSoldTotal += item2.getPrice();
 				break;
 			case 3:
-				itemsSoldTotal += 99.95;
+				itemsSoldTotal += item3.getPrice();
 				break;
 			case 4:
-				itemsSoldTotal += 350.89;
+				itemsSoldTotal += item4.getPrice();
 				break;				
 			default:
 				// Only 0-4 are valid choices
 				System.out.println("ERROR: Invalid input!");
 				break;
-			}
-    		
+			}  		
     		// Calculate commission and total earnings
         	commission = itemsSoldTotal * salesPerson.getCommission();
         	totalEarnings = commission + salesPerson.getSalary();
     	}
-    	
     	// Close scanner object
     	scanner.close();
     	// Display sales person total earnings
     	System.out.printf("Total earnings: %.2f",totalEarnings);
     }
-    
 }
--- Eclipse-Dev/CommisionCalculator/src/Item.java
+++ Eclipse-Dev/CommisionCalculator/src/Item.java
@@ -1,16 +1,39 @@
+/*
+ * Item.java
+ */
 
+/**
+ * 
+ * @author Radoslav Radoev
+ *
+ */
 public class Item {
 
+	/**
+	 * @param price 	Item price
+	 */
 	private double price;
 	
+	/**
+	 * Item constructor
+	 * @param price		Item price
+	 */
 	public Item(double price) {
 		setPrice(price);
 	}
 
+	/**
+	 * Item price getter
+	 * @return	Item price
+	 */
 	public double getPrice() {
 		return price;
 	}
 
+	/**
+	 * Item price setter
+	 * @param price		Item price
+	 */
 	public void setPrice(double price) {
 		this.price = price;
 	}
