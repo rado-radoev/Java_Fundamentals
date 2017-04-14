6d1e264f6552666ae6b4719c61b7d7ff7077ed21
superklamer <rradoev@gmail.com>
2017-04-13 20:51:06 -0700

    commission calculator

---
+++ Eclipse-Dev/CommisionCalculator/src/Item.java
@@ -0,0 +1,19 @@
+
+public class Item {
+
+	private double price;
+	
+	public Item(double price) {
+		setPrice(price);
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	
+	
+}
