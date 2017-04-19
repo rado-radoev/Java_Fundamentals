be8d466162c84f65796e438d4be3bd4dc25cb9d0
superklamer <rradoev@gmail.com>
2017-04-18 18:09:24 -0700

    Parking charges & DrawSmiley

---
+++ Eclipse-Dev/DrawSmiley/src/DrawBullsEye.java
@@ -0,0 +1,13 @@
+import java.awt.Graphics;
+import java.awt.Color;
+import javax.swing.JPanel;
+
+public class DrawBullsEye extends JPanel {
+	
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		
+		
+		
+	}
+}
