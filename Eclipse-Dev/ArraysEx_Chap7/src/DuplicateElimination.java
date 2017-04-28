da311d497f77b53814460062116fcc55f891d155
Radoslav Radoev <rradoev@gmail.com>
2017-04-27 17:51:55 -0700

    Ex 7.12 Duplicate elimination

--- Eclipse-Dev/ArraysEx_Chap7/src/DuplicateElimination.java
+++ Eclipse-Dev/ArraysEx_Chap7/src/DuplicateElimination.java
@@ -3,18 +3,38 @@ import java.util.Arrays;
 
 public class DuplicateElimination {
 
-	private SecureRandom randomNumber = new SecureRandom();
+	private static SecureRandom randomNumber = new SecureRandom();
 	
 	public static void main(String[] args) {
 		
 		int[] uniqueNumbers = new int[5];
+		int counter = 0;
+		
+		while (counter < 5) {
+			int num = 10 + randomNumber.nextInt(91);
+			
+			if (!searchArray2(uniqueNumbers, num)) {
+				uniqueNumbers[counter] = num;
+				counter++;
+			}
+		}
 
 	}
+	
+	private static boolean searchArray2(int[] array, int number) {
+		for (int i : array) {
+			if (i == number) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
 	
 	private static boolean searchArray(int[] arr, int number) {
 		// implement binary search
 		int i = 1;
-		int j = arr.length;
+		int j = arr.length - 1;
 		
 		while (i < j) {
 			int m = (i + j) / 2;
@@ -30,6 +50,8 @@ public class DuplicateElimination {
 		else
 			return false;
 	}
+	
+	
 	
 	private static int[] sortArray (int[] arr) {
 		// implement insertion sort
