3ce9d8cd5f96345a2999ad1e028727fc90bffea1
Radoslav Radoev <rradoev@gmail.com>
2017-04-28 23:47:53 -0700

    Sales People

--- Eclipse-Dev/ArraysEx_Chap7/TotalSales/SalesPerson.java
+++ Eclipse-Dev/ArraysEx_Chap7/TotalSales/SalesPerson.java
@@ -1,4 +1,39 @@
 
 public class SalesPerson {
 
+	private int[][] sales;
+	private static int personalNumber;
+	
+	public SalesPerson() {
+	}
+	
+	public int[][] getSales() {
+		return sales;
+	}
+
+	public int getPersonalNumber() {
+		return personalNumber;
+	}
+
+	public static void setPersonalNumber() {
+		personalNumber++;
+	}
+
+	public void setSales(int[][] sales) {
+		this.sales = sales;
+	}
+
+//	public static class SalesPersonBuilder {
+//		private int[][] sales;
+//		
+//		public SalesPersonBuilder (int[][] sales) {
+//			this.sales = sales;
+//		}
+//		
+//		public SalesPerson build() {
+//			SalesPerson salesPerson = new SalesPerson(this);
+//			return salesPerson;
+//		}
+//		
+//	}
 }
--- Eclipse-Dev/ArraysEx_Chap7/TotalSales/TotalSales.java
+++ Eclipse-Dev/ArraysEx_Chap7/TotalSales/TotalSales.java
@@ -2,18 +2,35 @@
 public class TotalSales {
 
 	public static void main(String[] args) {
+
+		SalesPerson[] salesPeople1 = {new SalesPerson(), new SalesPerson(), new SalesPerson(), new SalesPerson()};
+		int[] products1 = {100,200,300,400,500};
+		int[][]sales = new int[products1.length][1];
 		
-		int[] salesPeople = new int[4];
-		int[] products = {100,200,300,400,500};
-		int[][] sales = new int[products.length][salesPeople.length];
-		
-		for (int salesPerson = 0; salesPerson < salesPeople.length; salesPerson++) {
-			for (int product = 0; product < products.length; product++) {
-				sales[product][salesPerson] += products[product];
+		int count = 0;
+
+		for (SalesPerson sp : salesPeople1) {
+			sp.setSales(sales);
+			sp.setPersonalNumber();
+			while (count < 30) {
+				for (int product = 0; product < products1.length;product++){
+					sales[product][0] += products1[product]; 
+					sp.setSales(sales);
+				}
+				count++;
 			}
-		}
-		
-		
+
+			System.out.println("Sales rep N: " + sp.getPersonalNumber());
+			System.out.printf("%s%8s%n", "Product Number", "Product total");
+			for (int row = 0; row < sp.getSales().length; row++) {
+				System.out.printf("%d%8d%n", row + 1, sp.getSales()[row][0]);
+			}
+			
+			for (int i = 0; i < sales.length; i++) {
+				sales[i][0] = 0;
+			}
+			count = 0;
+		}		
 	}
 
 }
