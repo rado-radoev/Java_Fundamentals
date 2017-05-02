c2456743250075f974937f8f3e46645ac0b9ef9c
Radoslav Radoev <rradoev@gmail.com>
2017-05-01 21:57:54 -0700

    Junit test class

--- Eclipse-Dev/JUnitTests/Test/JUnitTester.java
+++ Eclipse-Dev/JUnitTests/Test/JUnitTester.java
@@ -1,12 +1,31 @@
 import static org.junit.Assert.*;
 
+import org.junit.FixMethodOrder;
 import org.junit.Test;
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+import org.junit.runner.notification.Failure;
+import org.junit.runners.MethodSorters;
 
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)	// sorts test cases by name
 public class JUnitTester {
+	
+	
+	public static void main(String[] args) {
+		Result result = JUnitCore.runClasses(JUnitTest.class);
+		for (Failure failure : result.getFailures()) {
+			System.out.println(failure.toString());
+		}
+	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
-	}
+    public void multiplicationOfZeroIntegersShouldReturnZero() {
+            JUnitTest tester = new JUnitTest(); // MyClass is tested
+
+            // assert statements
+            assertEquals("10 x 0 must be 0", 0, tester.multiply(10, 0));
+            assertEquals("0 x 10 must be 0", 0, tester.multiply(0, 10));
+            assertEquals("0 x 0 must be 0", 0, tester.multiply(0, 0));
+    }
 
 }
--- Eclipse-Dev/JUnitTests/src/JUnitTest.java
+++ Eclipse-Dev/JUnitTests/src/JUnitTest.java
@@ -1,4 +1,7 @@
 
 public class JUnitTest {
 
+	public int multiply(int a, int b) {
+		return 1;
+	}
 }
