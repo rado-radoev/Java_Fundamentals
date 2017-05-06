af27d9b4ef10bd31ff6f1c0b940c46f41ed670da
Radoslav Radoev <rradoev@gmail.com>
2017-05-06 16:40:43 -0700

    enhanced time2. not too happy about it, although it's working

--- Eclipse-Dev/ClassesObjects_Chap8/Test/Time2EnhancedTest.java
+++ Eclipse-Dev/ClassesObjects_Chap8/Test/Time2EnhancedTest.java
@@ -1,15 +1,19 @@
 // Fig. 8.6: Time2Test.java
 // Overloaded constructors used to initialize Time2 objects.
 
-public class Time2Test 
+public class Time2EnhancedTest 
 {
    public static void main(String[] args)
    {
-      Time2 t1 = new Time2(); // 00:00:00
-      Time2 t2 = new Time2(2); // 02:00:00
-      Time2 t3 = new Time2(21, 34); // 21:34:00
-      Time2 t4 = new Time2(12, 25, 42); // 12:25:42
-      Time2 t5 = new Time2(t4); // 12:25:42
+      Time2Enhanced t1 = new Time2Enhanced(); // 00:00:00
+      t1.tick();
+      Time2Enhanced t2 = new Time2Enhanced(2, 58); // 02:00:00
+      t2.incremenetMinute();
+      Time2Enhanced t3 = new Time2Enhanced(12, 34); // 21:34:00
+      t3.incrementHour();
+      Time2Enhanced t4 = new Time2Enhanced(12, 58, 59); // 12:25:42
+      t4.tick();
+      Time2Enhanced t5 = new Time2Enhanced(t4); // 12:25:42
 
       System.out.println("Constructed with:");
       displayTime("t1: all default arguments", t1);
@@ -21,7 +25,7 @@ public class Time2Test
       // attempt to initialize t6 with invalid values
       try
       {
-         Time2 t6 = new Time2(27, 74, 99); // invalid values
+         Time2Enhanced t6 = new Time2Enhanced(27, 74, 99); // invalid values
       } 
       catch (IllegalArgumentException e)
       {
@@ -31,7 +35,7 @@ public class Time2Test
    } 
 
    // displays a Time2 object in 24-hour and 12-hour formats
-   private static void displayTime(String header, Time2 t)
+   private static void displayTime(String header, Time2Enhanced t)
    {
       System.out.printf("%s%n   %s%n   %s%n",
          header, t.toUniversalString(), t.toString());
--- Eclipse-Dev/ClassesObjects_Chap8/src/Time2Enhanced.java
+++ Eclipse-Dev/ClassesObjects_Chap8/src/Time2Enhanced.java
@@ -1,54 +1,42 @@
 // Fig. 8.5: Time2.java
 // Time2 class declaration with overloaded constructors.  
 
-
-
-
-// TO DO:
-// Class should use seconds from midnight !!!!
-// remove 3 int, hour, second variables
-// modify getter and setter methods to use time from midnight in seconds
-
-
-
-
-
-
-
-
-public class Time2
+public class Time2Enhanced
 {
    private int hour; // 0 - 23
    private int minute; // 0 - 59
    private int second; // 0 - 59
-   
-   // seconds representation
-   private int timeInSeconds;
-   private final int SEC_IN_MIN = 60;
-   private final int SEC_IN_HOUR = SEC_IN_MIN * 60;
 
    // Time2 no-argument constructor: 
    // initializes each instance variable to zero
-   public Time2()
+   public Time2Enhanced()
    {
       this(0, 0, 0); // invoke constructor with three arguments
    } 
 
    // Time2 constructor: hour supplied, minute and second defaulted to 0
-   public Time2(int hour) 
+   public Time2Enhanced(int hour) 
    { 
       this(hour, 0, 0); // invoke constructor with three arguments
    } 
 
    // Time2 constructor: hour and minute supplied, second defaulted to 0
-   public Time2(int hour, int minute) 
+   public Time2Enhanced(int hour, int minute) 
    { 
       this(hour, minute, 0); // invoke constructor with three arguments
    } 
 
    // Time2 constructor: hour, minute and second supplied
-   public Time2(int hour, int minute, int second) 
+   public Time2Enhanced(int hour, int minute, int second) 
    { 
+      if (hour < 0 || hour >= 24)
+         throw new IllegalArgumentException("hour must be 0-23");
+
+      if (minute < 0 || minute >= 60)
+         throw new IllegalArgumentException("minute must be 0-59");
+
+      if (second < 0 || second >= 60)
+         throw new IllegalArgumentException("second must be 0-59");
 
       this.hour = hour;
       this.minute = minute; 
@@ -56,7 +44,7 @@ public class Time2
    } 
 
    // Time2 constructor: another Time2 object supplied
-   public Time2(Time2 time)
+   public Time2Enhanced(Time2Enhanced time)
    {
       // invoke constructor with three arguments
       this(time.getHour(), time.getMinute(), time.getSecond());
@@ -87,7 +75,7 @@ public class Time2
       if (hour < 0 || hour >= 24)
          throw new IllegalArgumentException("hour must be 0-23");
 
-      timeInSeconds += hour * SEC_IN_HOUR;
+      this.hour = hour;
    } 
 
    // validate and set minute 
@@ -96,7 +84,7 @@ public class Time2
       if (minute < 0 || minute >= 60)
          throw new IllegalArgumentException("minute must be 0-59");
 
-      timeInSeconds += minute * SEC_IN_MIN; 
+      this.minute = minute; 
    } 
 
    // validate and set second 
@@ -105,7 +93,7 @@ public class Time2
       if (second < 0 || second >= 60)
          throw new IllegalArgumentException("second must be 0-59");
 
-       timeInSeconds += second; 
+       this.second = second; 
    } 
 
    // Get Methods
@@ -141,4 +129,45 @@ public class Time2
          ((getHour() == 0 || getHour() == 12) ? 12 : getHour() % 12),
          getMinute(), getSecond(), (getHour() < 12 ? "AM" : "PM"));
    } 
+   
+   // add one second
+   public void tick() {
+	   if (getSecond() + 1 >= 60) {
+		   setSecond(0);	// if second + 1 will be 60 next second is 0
+		   if (getMinute() + 1 >= 60) {
+			   setMinute(0);	// if minute + 1 will be 60 next minute is 0
+			   if (getHour() + 1 >= 24) {
+				   setHour(0);	// if next hour is midnight set to 0
+			   }
+			   else setHour(getHour() + 1); // if not midnight increment hour
+		   }
+		   else setMinute(getMinute() + 1);  // if next minute is not 60, inrement it
+	   }
+	   else setSecond(getSecond() + 1);	// if next second is not 60, icrement it
+   }
+   // add one minute
+   public void incremenetMinute() {
+	   if (getMinute() + 1 >= 60) {
+		   setMinute(0);	// if minute + 1 will be 60, next minute is 0
+		   if (getHour() + 1 >= 24) {
+			   setHour(0); // if hour + 1 will be 24, next hour is 0
+		   }
+		   else {
+			   setHour(getHour() + 1);	// if hour + 1 is not 24, increment hour
+		   }
+	   }
+	   else {
+		   setMinute(getMinute() + 1);	// if minute + 1 is not 60, increment minute
+	   }
+   }
+   
+   // add one hour
+   public void incrementHour() {
+	   if (getHour() + 1 >= 24) {
+		   setHour(0); // if hour + 1 will be 24, next hour is 0
+	   }
+	   else {
+		   setHour(getHour() + 1);	// if hour + 1 is not 24, increment hour
+	   }
+   }
 } // end class Time2
