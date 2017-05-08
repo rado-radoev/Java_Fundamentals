a94986d58466eca9f8fcb79009b000beaf447b91
Radoslav Radoev <rradoev@gmail.com>
2017-05-07 22:02:06 -0700

    tic tac toe

--- Eclipse-Dev/ClassesObjects_Chap8/src/TicTacToe.java
+++ Eclipse-Dev/ClassesObjects_Chap8/src/TicTacToe.java
@@ -1,7 +1,30 @@
 // ex 8.17
+import java.util.Scanner;
+
 public class TicTacToe {
 
+	Scanner input = new Scanner(System.in);
+	
 	private TicTacToeEnum[][] board = new TicTacToeEnum[3][3];
+	
+	private boolean isEmpty(int row, int col) {
+		if (board[row][col] != TicTacToeEnum.EMPTY) return false;
+		else return true;
+	}
+	
+	private int[] userSelect() {
+		int[] userSelection = new int[2];
+		System.out.printf("Choose a row to play? (1-3): ");
+		int row = input.nextInt();
+		input.next();
+		System.out.printf("Choose a column to play? (1-3): ");
+		int column = input.nextInt();
+		
+		userSelection[0] = row;
+		userSelection[1] = column;
+		
+		return userSelection;
+	}
 	
 	public void displayBoard() {
 		System.out.printf("%s%s%s%n", "_______","_______","_______");
