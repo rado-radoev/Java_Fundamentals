c765e468e84c480037024d8d9170be59cacf7d59
Radoslav Radoev <rradoev@gmail.com>
2017-05-15 19:48:42 -0700

    Homework#7 - done

--- Eclipse-Dev/Homework#7/IntegerSet/src/IntegerSet.java
+++ Eclipse-Dev/Homework#7/IntegerSet/src/IntegerSet.java
@@ -12,14 +12,15 @@ public class IntegerSet {
      * Creates a new instance of IntegerSet   
      */
 	private boolean[] set = new boolean[101];
-	private static boolean[] unionSet = new boolean[101];
-    
-	// Default constructor
+   
+	/**
+	 * Default constructor
+	 */
 	public IntegerSet() {}
     
     /**
      *  IntegerSet accessor
-	 * @return the set
+	 * @return the set as a boolean array
 	 */
 	private boolean[] getSet() {
 		return set;
@@ -30,50 +31,56 @@ public class IntegerSet {
 	 * @param 	sets the boolean array set 
 	 */
 	private void setSet(boolean[] set) {
+		// Throw an error message if a larger set is passed
+		if (set.length > this.set.length) {
+			throw new IndexOutOfBoundsException("Set should be in range 0 - 100");
+		}
+		
 		this.set = set;
 	}
 
+
 	/**
-     * Return a new IntegerSet containing the union of the two IntegerSet objects
-     * passed as arguments
-     */
-	public static IntegerSet union (IntegerSet a, IntegerSet b) {	
+	 * Outputs the union between two sets, passed as arguments
+	 * @param a	First set to compare
+	 * @param b	Second set to compare
+	 * @return	Returns a new IntegerSet, containing the union of the two IntegerSet objects
+	 */
+	public static IntegerSet union (IntegerSet a, IntegerSet b) {
+		// create a new IntegerSet that will be hold the union of the two integer sets
+		IntegerSet resultSet = new IntegerSet();	
 		
-		IntegerSet temp = new IntegerSet();
-		// iterate through array a.length. - both arrays should have the same size 101
-		// if a[i] == b[i], if both are true then add one element to new array
-		// if a[i] == true, add it to the array
-		// if b[i] == true, add it to the array
-		// if any of the elements is set to false, they are not in the set, so 
-		// don't add them to the new boolean array;
-		
-		for (int i = 0; i < unionSet.length; i++) {
-			if (a.set[i] == b.set[i]) {
-				unionSet[i] = true;
-			}
-			else if (a.set[i] == true) {
-				unionSet[i] = true;
-			}
-			else if (b.set[i] == true) {
-				unionSet[i] = true;
-			}
+		for (int i = 0; i < resultSet.getSet().length; i++) {
+			if (a.set[i] || b.set[i]) {		// if element in a OR element in b are true
+				resultSet.set[i] = true;
+			}	
 		}
-		
-		return unionSet;
-		// For example: let A = (1,2,3) and let B = (3,4,5). Now the UNION of A and B, written A union B = (1,2,3,4,5). There is no need to list the 3 twice.
+		return resultSet;
 	}
 	
-    
-    /**
-     * Return a new IntegerSet containing the intersection of the two IntegerSet objects
-     * passed as arguments
-     */
-    // TODO: implement the intersection method
+	/**
+	 * Outputs the intersection of two sets, passed as arguments
+	 * @param a	First set to compare
+	 * @param b	Second set to compare
+	 * @return Returns a new IntegerSet containing the intersection of the two IntegerSet objects
+	 */
+    public static IntegerSet intersection(IntegerSet a, IntegerSet b) {
+    	// create a new IntegerSet that will be hold the intersection of the two integer sets
+    	IntegerSet resultSet = new IntegerSet();
+    	
+    	for (int i = 0; i < resultSet.getSet().length; i++) {
+    		if (a.set[i] && b.set[i]) {		// if element in a AND element in b are true
+    			resultSet.set[i] = true;
+    		}
+    	}
+    	return resultSet;
+    }
     
     /**
      * Inserts an element into the IntegerSet by setting the corresponding
-     * value within the set array to true. Returns false if the value was out
-     * of range and true otherwise.
+     * value within the set array to true
+     * @param index	The array index to set
+     * @return Returns false if the value was out of range and true otherwise.
      */
     public boolean insertElement(int index) {   	
     	if (index > set.length || index < 0) {
@@ -87,8 +94,9 @@ public class IntegerSet {
     
     /**
      * Deletes an element from the IntegerSet by setting the corresponding
-     * value within the set array to false. Returns false if the value was out
-     * of range and true otherwise.
+     * value within the set array to false. 
+     * @param index	The array index to set
+     * @return Returns false if the value was out of range and true otherwise.
      */
     public boolean deleteElement(int index) {   	
     	if (index > set.length || index < 0) {
@@ -101,18 +109,53 @@ public class IntegerSet {
     }
     
     /**
-     * @Override the toString method in the Object class
+     * Overrides the toString method in the Object class
      * Displays the integers contained by the IntegerSet separated by spaces. 
-     * An empty set should be displayed as:
-     * { --- }
-     * An integer set containing 5 and 10 should be displayed as:
-     * { 5 10 }
      */
-    // TODO: implement the toString method   
+    @Override
+    public String toString() {
+    	StringBuilder sb = new StringBuilder();
+    	
+    	for (int i = 0; i < this.set.length; i++) {
+    		if (this.set[i]) {	// Loops through the array and if an element is set to true add to output string
+    			sb.append(String.format("%d ", i));	
+    		}
+    	}
+    	
+    	if (sb.length() > 0) {
+    		return String.format("{ %s} ", sb.toString()); // Display integers in the format of { 5 10 }
+    	}
+    	else {
+    		return String.format("{ --- }");	// Display when empty set
+    	}
+    }
     
     /**
-     * Returns true iff the current IntegerSet contains the same integers as
+     * Checks if current set is equal to another set passed as argument
+     * @param Set to check for equality
+     * @return Returns true if the current IntegerSet contains the same integers as
      * the IntegerSet supplied as an argument
      */
-	// TODO: implement the isEqualTo method         
+	public boolean isEqualTo(IntegerSet b) {
+		// Loop trough both sets and as soon as a difference if find return false
+		// If no difference is found return true. Both sets are equal
+		for (int i = 0; i < this.set.length; i++) {
+			if (this.set[i] != b.set[i]) {
+				return false;	// the two sets are not equal
+			}
+		}
+		return true;	// the two sets are equal
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
