2972540c81e5c0a66409bf546d66f06594784797
Radoslav Radoev <rradoev@gmail.com>
2017-05-28 12:48:42 -0700

    Urlify algorithm

--- Eclipse-Dev/URLify/src/main/java/com/urlify/URLify.java
+++ Eclipse-Dev/URLify/src/main/java/com/urlify/URLify.java
@@ -27,4 +27,26 @@ public class URLify {
 			}
 		}
 	}
+	
+	
+	
+	public static String replaceString(String str) {
+		StringBuilder sb = new StringBuilder();
+		
+		char[] chars = str.toCharArray();
+		for (int i = 0; i < chars.length; i++) {
+			if (chars[i] == ' ') {
+				sb.append("%20");
+			} else {
+				sb.append(chars[i]);
+			}
+		}
+		
+		return sb.toString();
+	}
+	
+	public static void main(String[] args) {
+		String str = "Mr John Smith    ";
+		System.out.println(replaceString(str));
+	}
 }
