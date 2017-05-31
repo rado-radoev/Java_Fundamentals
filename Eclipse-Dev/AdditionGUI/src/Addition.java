288caeafcea26dd5a2a8dd2aecbd4be9b7e19e37
Radoslav Radoev <rradoev@gmail.com>
2017-05-30 21:12:30 -0700

    simple gui app

---
+++ Eclipse-Dev/AdditionGUI/src/Addition.java
@@ -0,0 +1,24 @@
+import javax.swing.JOptionPane;
+
+public class Addition {
+
+	public static void main(String[] args) {
+		// obtain user input from JOptionPane input dialogs
+		String firstNumber = 
+				JOptionPane.showInputDialog("Enter first integer");
+		
+		String secondNumber = 
+				JOptionPane.showInputDialog("Enter second integer");
+		
+		// convert String input to int values for use in a calculation
+		int number1 = Integer.parseInt(firstNumber);
+		int number2 = Integer.parseInt(secondNumber);
+		
+		int sum = number1 + number2;
+		
+		// display result in a JOption message dialog
+		JOptionPane.showMessageDialog(null, "The sum is " + sum, "Sum of two Integers", JOptionPane.PLAIN_MESSAGE);
+		
+		
+	}
+}
