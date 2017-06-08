34dd811280c38e56aea36ee91f8a5df2b6a6cdf1
Radoslav Radoev <rradoev@gmail.com>
2017-06-08 12:21:41 -0700

    ex 12.8 - almost done

--- Eclipse-Dev/AdditionGUI/src/com/ex128/Align.java
+++ Eclipse-Dev/AdditionGUI/src/com/ex128/Align.java
@@ -7,19 +7,19 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
+import java.awt.Font;
 import javax.swing.JPanel;
 import javax.swing.JFrame;
 import javax.swing.JCheckBox;
 import javax.swing.JButton;
 import javax.swing.JTextField;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+
 
 
 public class Align extends JFrame {
 
-	private final GridLayout buttonsGrid;
-	private final GridLayout textFieldsGrid;
-	private final GridLayout checkBoxesGrid;
 	private final JTextField[] textFields;
 	private final JLabel[] labels;
 	private final JCheckBox[] checkBoxes;
@@ -29,9 +29,10 @@ public class Align extends JFrame {
 	private final String[] buttonNames = {"OK", "Cancel", "Help"};
 	private final JPanel jpanelButtons;
 	private final JPanel jpanelCheckBoxes;
-	private final JPanel textFields;
+	private final JPanel jpanelTextFields;
 	private final Container container;
 	private final FlowLayout layout;
+
 	
 	public Align() {
 		
@@ -39,8 +40,99 @@ public class Align extends JFrame {
 		layout = new FlowLayout();
 		layout.setHgap(10);
 		layout.setVgap(5);
-	
+		setLayout(layout);
 		
+		container = getContentPane();
+	
+		jpanelButtons = new JPanel(new GridLayout(3, 1, 5, 5));
+		jpanelCheckBoxes = new JPanel(new GridLayout(2, 1, 1, -5));
+		jpanelTextFields = new JPanel(new GridLayout(2, 2, -40, 15));
+		
+	
+		// Add checkboxes to JPanel
+		CheckBoxHandler checkBoxHandler = new CheckBoxHandler();
+		checkBoxes = new JCheckBox[checkBoxesNames.length];
+		for (int i = 0; i < checkBoxesNames.length; i++) {
+			checkBoxes[i] = new JCheckBox(checkBoxesNames[i]);
+			checkBoxes[i].addItemListener(checkBoxHandler);
+			jpanelCheckBoxes.add(checkBoxes[i]);
+		}
+		
+		// Generate text fields with labels
+		textFields = new JTextField[labelNames.length];
+		labels = new JLabel[labelNames.length];
+		for (int i = 0; i < labelNames.length ;i++) {
+			textFields[i] = new JTextField("8", 5);
+			labels[i] = new JLabel(labelNames[i] + ":");
+			jpanelTextFields.add(labels[i]);
+			jpanelTextFields.add(textFields[i]);
+		}
+			
+		// Add buttons to JPanel
+		ButtonHandler handler = new ButtonHandler();
+		buttons = new JButton[buttonNames.length];
+		for (int i = 0; i < buttonNames.length; i++) {
+			buttons[i] = new JButton(buttonNames[i]);
+			buttons[i].addActionListener(handler);
+			jpanelButtons.add(buttons[i]);
+		}
+		
+	
+		add(jpanelCheckBoxes);
+		add(jpanelTextFields);
+		add(jpanelButtons);
+	}
+	
+	private class CheckBoxHandler implements ItemListener {
+		@Override
+		public void itemStateChanged(ItemEvent e) {
+			JOptionPane.showMessageDialog(Align.this, "Checkbox checked");
+		}
+	}
+	
+	private class ButtonHandler implements ActionListener {
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			JOptionPane.showMessageDialog(Align.this, "You pressed " + e.getActionCommand());
+			
+		}
 		
 	}
+	
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- Eclipse-Dev/AdditionGUI/src/com/ex128/AlignTest.java
+++ Eclipse-Dev/AdditionGUI/src/com/ex128/AlignTest.java
@@ -6,6 +6,10 @@ public class AlignTest {
 
 	public static void main(String[] args) {
 
+		Align alignFrame = new Align();
+		alignFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		alignFrame.setSize(350, 135);
+		alignFrame.setVisible(true);
 
 	}
 
