690e06ecc8ab525a8e7b2be091891d9ee7773921
Radoslav Radoev <rradoev@gmail.com>
2017-06-08 21:24:01 -0700

    blah

---
+++ Eclipse-Dev/AdditionGUI/src/com/ex1210/ColorSelect.java
@@ -0,0 +1,59 @@
+package com.ex1210;
+
+import java.awt.FlowLayout;
+import java.awt.Color;
+import java.awt.Font;
+import javax.swing.JFrame;
+import javax.swing.JList;
+import javax.swing.JCheckBox;
+import javax.swing.JButton;
+import javax.swing.JScrollPane;
+import javax.swing.event.ListSelectionListener;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.ListSelectionModel;
+
+
+public class ColorSelect {
+
+	private final Font font;
+	private final Color[] = {/* all the colors*/};
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Eclipse-Dev/AdditionGUI/src/com/ex1210/ColorSelectTest.java
@@ -0,0 +1,10 @@
+package com.ex1210;
+
+public class ColorSelectTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
