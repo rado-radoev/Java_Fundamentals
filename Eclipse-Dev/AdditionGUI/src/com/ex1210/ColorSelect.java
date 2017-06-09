866bcc88a5dd27be3a29e57da17319ecc242ab78
Radoslav Radoev <rradoev@gmail.com>
2017-06-08 21:53:24 -0700

    ex 1210

--- Eclipse-Dev/AdditionGUI/src/com/ex1210/ColorSelect.java
+++ Eclipse-Dev/AdditionGUI/src/com/ex1210/ColorSelect.java
@@ -1,6 +1,7 @@
 package com.ex1210;
 
 import java.awt.FlowLayout;
+import java.awt.Checkbox;
 import java.awt.Color;
 import java.awt.Font;
 import javax.swing.JFrame;
@@ -13,23 +14,38 @@ import javax.swing.event.ListSelectionEvent;
 import javax.swing.ListSelectionModel;
 
 
-public class ColorSelect {
+public class ColorSelect extends JFrame {
 
 	private final Font font;
-	private final Color[] = {/* all the colors*/};
-
-
-
-
-
-
-
-
-
-
-
+	private final Color[] colors = {Color.BLACK, Color.BLUE, Color.CYAN, Color.GRAY, Color.DARK_GRAY, Color.MAGENTA, Color.GREEN, Color.ORANGE, Color.PINK,
+			Color.RED, Color.WHITE, Color.YELLOW};
+	private final String[] colorNames = {"Black", "Blue", "Cyan", "Gray", "Dark Gray", "Magenta", "Green", "Orange", "Pink", "Red", "White", "Yellow"};
+	private final JCheckBox boldCheckBox;
+	private final JCheckBox italicCheckBox;
+	private final JButton ok;
+	private final JButton cancel;
+	private final JList colorList;
+	
+	public ColorSelect() {
+		super("Color select");
+		setLayout(new FlowLayout());
+		
+		boldCheckBox = new JCheckBox("Bold");
+		italicCheckBox = new JCheckBox("Italic");
+		
+		ok = new JButton("OK");
+		cancel = new JButton("Cancel");
+		
+		
+	}
 
 }
+
+
+
+
+
+
 
 
 
