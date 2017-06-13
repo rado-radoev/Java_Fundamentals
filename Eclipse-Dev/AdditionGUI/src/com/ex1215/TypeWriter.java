039e0b5162b5fb16cd4f5a510811e19801a5814e
Radoslav Radoev <rradoev@gmail.com>
2017-06-13 14:55:50 -0700

    TypeWriter backspace and shift disappear when releasing the shift key and noth highlighting when pressed

--- Eclipse-Dev/AdditionGUI/src/com/ex1215/TypeWriter.java
+++ Eclipse-Dev/AdditionGUI/src/com/ex1215/TypeWriter.java
@@ -11,17 +11,20 @@ import javax.swing.JButton;
 import java.awt.GridLayout;
 import java.awt.LayoutManager;
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.FlowLayout;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.awt.event.KeyListener;
 import java.awt.event.KeyEvent;
+import java.util.HashMap;
 
-public class TypeWriter extends JFrame {
+public class TypeWriter extends JFrame implements KeyListener {
 	
 	//private final JTextArea textArea;
 	private final JButton[] letterButtons = new JButton[127]; // array that will contain the number of letters from z to a
 	private final Character[] chars = new Character[letterButtons.length];
+	private final HashMap<String, JButton> buttonMap;
 	private final JPanel buttons;
 	private final JTextArea textArea;
 	private final BorderLayout textAreaBorderLayout;
@@ -46,53 +49,86 @@ public class TypeWriter extends JFrame {
 		textAreaBorderLayout = new BorderLayout(10, 45);
 		textAreaJpanel = new JPanel();
 		textArea = new JTextArea(textAreaBorderLayout.getHgap(), textAreaBorderLayout.getVgap());
-		textArea.setEditable(false);
+		textArea.setEditable(true);
 		textArea.setVisible(true);
+		textArea.addKeyListener(this);
+		textArea.setLineWrap(true);
+		textArea.setWrapStyleWord(true);
 		textAreaJpanel.add(textArea, textAreaBorderLayout);
 		
 		
 		buttons = new JPanel();
-		KeyHandler handler = new KeyHandler();
+		buttonMap = new HashMap<String, JButton>();
 		for (int i = 97; i <= 122; i++) {
 			chars[i] = (char)i;
 			letterButtons[i] = new JButton(chars[i].toString());
 			textAreaJpanel.add(letterButtons[i], textAreaBorderLayout.SOUTH);
+			letterButtons[i].addKeyListener(this);
+			buttonMap.put(chars[i].toString(), letterButtons[i]);
+			
 			
 			if (i == 105 || i == 73) {
 				letterButtons[8] = new JButton("Backsp");
 				textAreaJpanel.add(letterButtons[8], textAreaBorderLayout.SOUTH);
+				letterButtons[8].addKeyListener(this);
+				buttonMap.put("Backsp", letterButtons[8]);
+			}
+			
+			
+			if (i == 116 || i == 84) {
+				letterButtons[14] = new JButton("Shift     ");
+				textAreaJpanel.add(letterButtons[14], textAreaBorderLayout.SOUTH);
+				letterButtons[14].addKeyListener(this);
+				buttonMap.put("Backsp", letterButtons[14]);
 			}
 		}
 		
 		letterButtons[32] = new JButton("Space                                                                             ");
 		textAreaJpanel.add(letterButtons[32]);
-		
-		
 
 		add(upperTextArea, BorderLayout.NORTH);
 		add(textAreaJpanel, BorderLayout.CENTER);
 
 	}
 	
-	private class KeyHandler implements KeyListener {
-
-		@Override
-		public void keyTyped(KeyEvent e) {
-			textArea.setText(e.getKeyText(e.getKeyChar()));		
+	@Override
+	public void keyTyped(KeyEvent e) {
+		JButton button = buttonMap.get(String.valueOf(e.getKeyChar()));
+		if (button != null) {
+			button.doClick();
+		}
+		if (e.getKeyCode() == KeyEvent.VK_BACK_SPACE) {
+			textArea.setText(textArea.getText().substring(0, textArea.getText().length() - 1));
 		}
 
-		@Override
-		public void keyPressed(KeyEvent e) {
-			textArea.setText(e.getKeyText(e.getKeyChar()));	
-			
-		}
+	}
 
-		@Override
-		public void keyReleased(KeyEvent e) {
-			// TODO Auto-generated method stub
-			
+	@Override
+	public void keyPressed(KeyEvent e) {
+		if (e.getKeyCode() == KeyEvent.VK_SHIFT) {
+			for (JButton button : letterButtons) {
+				if (button != null) {
+					if (button.getText().charAt(0) >= 97 && button.getText().charAt(0) <= 122) {
+						button.setText(String.valueOf(String.valueOf((char)(button.getText().charAt(0) - 32))));
+					}					
+				}
+			}
+			textAreaJpanel.repaint();
 		}
-		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		if (e.getKeyCode() == KeyEvent.VK_SHIFT) {
+			for (JButton button : letterButtons) {
+				if (button != null) {
+					if (button.getText().charAt(0) >= 65 && button.getText().charAt(0) <= 90) {
+						button.setText(String.valueOf(String.valueOf((char)(button.getText().charAt(0) + 32))));
+					}					
+				}
+			}
+			textAreaJpanel.repaint();
+		}
 	}
 
 }
