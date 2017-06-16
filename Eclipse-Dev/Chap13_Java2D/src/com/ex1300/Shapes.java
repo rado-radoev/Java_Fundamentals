89ae2e946d4841bab8bc9f3b72ff6d5d87fbd1c2
Radoslav Radoev <rradoev@gmail.com>
2017-06-15 21:11:49 -0700

    more shapes testing

--- Eclipse-Dev/Chap13_Java2D/src/com/ex1300/Shapes.java
+++ Eclipse-Dev/Chap13_Java2D/src/com/ex1300/Shapes.java
@@ -5,6 +5,8 @@ import java.awt.Graphics;
 import java.awt.Graphics2D;
 import javax.swing.JPanel;
 import java.awt.BasicStroke;
+import java.awt.Font;
+import javax.swing.JColorChooser;
 
 public class Shapes extends JPanel {
 	
@@ -12,9 +14,18 @@ public class Shapes extends JPanel {
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
 		
-		g.setColor(Color.GREEN);
-		g.drawLine(1, 260, 280, 1);
-		g.setColor(Color.RED);
-		g.drawLine(280, 1, 1, 260);
+//		g.setColor(Color.BLACK);
+//		g.drawLine(0, 300, 300, 0);
+//		g.setColor(new Color(255,0,0));
+//		g.drawLine(300, 300, 0, 0);
+		
+		Font f = new Font(Font.MONOSPACED, Font.BOLD + Font.ITALIC, 14)
+		g.setFont(f);
+		//Color c = JColorChooser.showDialog(this, "Избери цвят", new Color(100,200,255));
+		String s = "Хакуна матата";
+		g.drawString(s, 100, 50);
+		g.drawString("String length is " + s.length() + "Font style is " + f.getStyle() + 
+				"Font name is " + f.getName() + "Font size is " + f.getSize() + 
+				"Font family is " + f.getFamily() + ,  x, y);
 	}
 }
