372eaa00e0896dd8db463f437b10a8c94b7df4e0
Radoslav Radoev <rradoev@gmail.com>
2017-06-16 14:35:11 -0700

    more raodblocks

---
+++ Eclipse-Dev/Chap13_Java2D/src/com/ex1300/Eyes.java
@@ -0,0 +1,14 @@
+package com.ex1300;
+
+import java.awt.Graphics;
+
+import java.awt.Graphics2D;
+
+public class Eyes extends Shapes {
+
+	private Graphics g;
+	private Graphics2D leftEye = (Graphics2D) g;
+	private Graphics2D rightEye = (Graphics2D) g;
+	private Graphics2D leftIris = (Graphics2D) g;
+	private Graphics2D rightIris = (Graphics2D) g;
+}
--- Eclipse-Dev/Chap13_Java2D/src/com/ex1300/Shapes.java
+++ Eclipse-Dev/Chap13_Java2D/src/com/ex1300/Shapes.java
@@ -3,12 +3,18 @@ package com.ex1300;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.Shape;
+import java.awt.geom.Ellipse2D;
+import java.nio.channels.InterruptedByTimeoutException;
+
 import javax.swing.JPanel;
 import java.awt.BasicStroke;
 import java.awt.Font;
+import java.awt.GradientPaint;
+
 import javax.swing.JColorChooser;
 
-public class Shapes extends JPanel {
+public class Shapes extends JPanel  {
 	
 	@Override
 	public void paintComponent(Graphics g) {
@@ -18,14 +24,45 @@ public class Shapes extends JPanel {
 //		g.drawLine(0, 300, 300, 0);
 //		g.setColor(new Color(255,0,0));
 //		g.drawLine(300, 300, 0, 0);
+//		
+//		Font f = new Font(Font.MONOSPACED, Font.BOLD + Font.ITALIC, 14);
+//		g.setFont(f);
+//		String s = "Хакуна Матата!";
+//		g.drawString(s, 100, 50);
+//		g.setColor(new Color(255,10,10));
+//		g.drawString("String length is " + s.length() + "Font style is " + f.getStyle() + 
+//				"Font name is " + f.getName() + "Font size is " + f.getSize() + 
+//				"Font family is " + f.getFamily(),  50, 150);
 		
-		Font f = new Font(Font.MONOSPACED, Font.BOLD + Font.ITALIC, 14)
-		g.setFont(f);
-		//Color c = JColorChooser.showDialog(this, "Избери цвят", new Color(100,200,255));
-		String s = "Хакуна матата";
-		g.drawString(s, 100, 50);
-		g.drawString("String length is " + s.length() + "Font style is " + f.getStyle() + 
-				"Font name is " + f.getName() + "Font size is " + f.getSize() + 
-				"Font family is " + f.getFamily() + ,  x, y);
+		Graphics2D leftEye = (Graphics2D) g;
+		Graphics2D rightEye = (Graphics2D) g;
+		Graphics2D leftIris = (Graphics2D) g;
+		Graphics2D rightIris = (Graphics2D) g;
+	
+		for (int i = 0; i < 10; i++) {
+			leftEye.setPaint(Color.WHITE);
+			leftEye.fill(new Ellipse2D.Double(5, 30, 70, 100));
+			rightEye.setPaint(Color.WHITE);
+			rightEye.fill(new Ellipse2D.Double(70, 30, 70, 100));
+			
+			leftIris.setPaint(Color.BLACK);
+			leftIris.fill(new Ellipse2D.Double(20, 40, 40, 40));
+			rightIris.setPaint(Color.BLACK);
+			rightIris.fill(new Ellipse2D.Double(85, 40, 40, 40));
+
+			leftEye.setPaint(Color.WHITE);
+			leftEye.fill(new Ellipse2D.Double(5, 30, 70, 100));
+			rightEye.setPaint(Color.WHITE);
+			rightEye.fill(new Ellipse2D.Double(70, 30, 70, 100));
+			
+			leftIris.setPaint(Color.BLACK);
+			rightIris.setPaint(Color.BLACK);
+			rightIris.fill(new Ellipse2D.Double(20, 80, 40, 40));
+			leftIris.fill(new Ellipse2D.Double(85, 80, 40, 40));
+			
+		}
+
+
 	}
+
 }
