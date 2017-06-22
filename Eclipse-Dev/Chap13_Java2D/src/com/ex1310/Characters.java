cd0378979f555ab53139c6dacd8d84f4e8261b84
Radoslav Radoev <rradoev@gmail.com>
2017-06-21 17:57:54 -0700

    random characters

--- Eclipse-Dev/Chap13_Java2D/src/com/ex1310/Characters.java
+++ Eclipse-Dev/Chap13_Java2D/src/com/ex1310/Characters.java
@@ -17,10 +17,9 @@ public class Characters extends JPanel {
 		super.paintComponent(g);
 		
 		rand = new Random(System.currentTimeMillis());
-		c = (char) (rand.nextInt(80) + 32);
 		
-		for (int i = 0; i < 10; i++) {
-			c = (char) (rand.nextInt(80) + 32);
+		for (int i = 0; i < 50; i++) {
+			c = (char) (rand.nextInt(25) + 97);
 			g.setColor(new Color(rand.nextInt(255), rand.nextInt(255), rand.nextInt(255)));
 			g.setFont(new Font(fontFamily[rand.nextInt(fontFamily.length)], rand.nextInt(4), (rand.nextInt(50) + 1)));
 			g.drawString(String.valueOf(c), rand.nextInt(400), rand.nextInt(400));
