4d52139b8ca78bf9b0e9e39f168c1dfc613a453a
Radoslav Radoev <rradoev@gmail.com>
2017-06-21 21:20:26 -0700

    DrawCircle with user prompt -wip

--- Eclipse-Dev/Chap13_Java2D/src/com/ex1317/DrawCircleEllipse.java
+++ Eclipse-Dev/Chap13_Java2D/src/com/ex1317/DrawCircleEllipse.java
@@ -1,6 +1,19 @@
 package com.ex1317;
 
-public class DrawCircleEllipse {
+import javax.swing.JPanel;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.geom.Ellipse2D;
 
+public class DrawCircleEllipse extends JPanel {
+
+	@Override
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		
+		Graphics2D g2d = (Graphics2D) g;
+		
+		
+	}
 	
 }
