41f3d6e9e2bfc3334e43fdf31d6c7a5149859b0b
Radoslav Radoev <rradoev@gmail.com>
2017-06-22 19:20:34 -0700

    13.17 done

---
+++ Eclipse-Dev/Chap13_Java2D/src/com/ex1317/EllipseFrame.java
@@ -0,0 +1,12 @@
+package com.ex1317;
+
+import javax.swing.JFrame;
+
+public class EllipseFrame extends JFrame {
+
+	public EllipseFrame() {
+		setSize(500, 500);
+		setVisible(true);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+}
