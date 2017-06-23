3aab30b4eee484b6abd9b8eb43f9897b3b665d7a
Radoslav Radoev <rradoev@gmail.com>
2017-06-22 20:27:14 -0700

    screen saver

--- Eclipse-Dev/Chap13_Java2D/src/com/ex1318/ScreenSaver.java
+++ Eclipse-Dev/Chap13_Java2D/src/com/ex1318/ScreenSaver.java
@@ -3,6 +3,7 @@ package com.ex1318;
 import javax.swing.JPanel;
 import java.awt.Graphics;
 import java.awt.Color;
+import java.awt.Container;
 import java.util.Random;
 
 
@@ -14,13 +15,18 @@ public class ScreenSaver extends JPanel {
 	@Override
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
+	
 		
 		rnd = new Random(System.currentTimeMillis());
 		
 		for (int i = 0; i < 101; i++) {
 			g.setColor(new Color(rnd.nextInt(256), rnd.nextInt(256), rnd.nextInt(256)));
-			g.drawLine(rnd.nextInt(200), rnd.nextInt(400), rnd.nextInt(200), rnd.nextInt(400));
+			g.drawLine(rnd.nextInt(this.getParent().getSize().width), 
+					rnd.nextInt(this.getParent().getSize().height),
+					rnd.nextInt(this.getParent().getSize().width),
+					rnd.nextInt(this.getParent().getSize().height));
 		}
+		
 		
 		repaint();
 		
