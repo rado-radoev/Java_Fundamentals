82a16c11e4c133be84c4bf764921677256375af9
Radoslav Radoev <rradoev@gmail.com>
2017-06-23 18:30:46 -0700

    random shapes

---
+++ Eclipse-Dev/Chap13_Java2D/src/com/ex1321_abandoned/ShapesGenerator.java
@@ -0,0 +1,36 @@
+package com.ex1321_abandoned;
+
+import javax.swing.JPanel;
+import java.awt.Graphics;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.FlowLayout;
+
+import javax.swing.Timer;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.util.Random;
+import java.awt.Shape;
+import javax.swing.JTextField;
+import javax.swing.JLabel;
+import javax.swing.JButton;
+
+public class ShapesGenerator extends JPanel {
+	
+	
+	
+	@Override
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		
+//		switch (ScreenSaver.getUserChoice()) {
+//		case value:
+//			
+//			break;
+//
+//		default:
+//			break;
+//		}
+	}
+
+}
