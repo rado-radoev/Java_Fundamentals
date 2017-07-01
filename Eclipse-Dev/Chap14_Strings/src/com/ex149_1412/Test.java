045dfe3b2d058b8b842def71ab9fe0bc05aca042
Radoslav Radoev <rradoev@gmail.com>
2017-06-30 18:25:07 -0700

    ex.14.12 logic almost done. finds and empty char at the beginning. need to modify system.out to string.format

--- Eclipse-Dev/Chap14_Strings/src/com/ex149_1412/Test.java
+++ Eclipse-Dev/Chap14_Strings/src/com/ex149_1412/Test.java
@@ -4,16 +4,31 @@ public class Test {
 
 	public static void main(String[] args) {
 		int[] chars = new int[128];
-		String text = "this is a test";
-		char c = 't';
+		boolean[] found = new boolean[128];
 		
-		int start = text.indexOf(c, 0);
-		while (start > -1) {
-			chars[c]++;
-			start = text.indexOf(c, start + 1);
+		String text = "this is a test";
+		
+		for (int i = 0; i < text.length(); i++) {
+			int start = text.indexOf(text.charAt(i), 0);
+			while (start > -1) {
+				if (found[text.charAt(i)] != true) {
+					chars[text.charAt(i)]++;	
+				}
+				
+				start = text.indexOf(text.charAt(i), start + 1);
+			}
+			found[text.charAt(i)] = true;
 		}
+		
+		for (int i = 1; i < chars.length; i++) {
+			if (chars[i] > 0) {
+				System.out.println("character " + (char)i + " is found: " + chars[i] + " times");
+			}
+		}
+		
+
  		
-		System.out.println(String.format("Character %s, found: %d times", Character.valueOf(c), chars[c]));
+		//System.out.println(String.format("Character %s, found: %d times", Character.valueOf(c), chars[c]));
 
 	}
 
