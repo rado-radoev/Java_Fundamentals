9c911f6fbef9d18f3a80f6e9cd45bbc850ff6a9d
Radoslav Radoev <rradoev@gmail.com>
2017-07-02 12:40:13 -0700

    Stupd java. I'll never learn it. Why did I started with this shit

---
+++ Eclipse-Dev/Chap15_Files/src/com/CreditInquiry/MenuOption.java
@@ -0,0 +1,16 @@
+package com.CreditInquiry;
+
+public enum MenuOption {
+
+	// declare contents of enum type
+	ZERO_BALANCE(1),
+	CREDIT_BALANCE(2),
+	DEBIT_BALANCE(3),
+	END(4);
+	
+	private final int value;	// current menu option
+	
+	private MenuOption(int value) {
+		this.value = value;
+	}
+}
