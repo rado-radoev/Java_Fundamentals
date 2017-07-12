6df1869f70020956349c4328ba4d85f1e1dcef97
Radoslav Radoev <rradoev@gmail.com>
2017-07-12 16:29:53 -0700

    Chap16_Generics

---
+++ Eclipse-Dev/Chap16_Generics/src/com/fig164/UsingToArray.java
@@ -0,0 +1,28 @@
+package com.fig164;
+
+import java.util.LinkedList;
+import java.util.Arrays;
+
+public class UsingToArray {
+
+	public static void main(String[] args) {
+		String[] colors = {"blac", "blue", "yellow"};
+
+		LinkedList<String> links = new LinkedList<>(Arrays.asList(colors));
+		
+		links.addLast("red");
+		links.add("pink");
+		links.add(3, "green");
+		links.addFirst("cyan");
+		
+		colors = links.toArray(new String[links.size()]);
+		
+		System.out.println("colors: ");
+		
+		for (String color: colors) {
+			System.out.println(color);
+		}
+		
+	}
+
+}
