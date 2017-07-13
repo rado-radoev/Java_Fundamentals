f2755312850d3aa515817c04c08501acd42ab5df
Radoslav Radoev <rradoev@gmail.com>
2017-07-13 07:30:13 -0700

    Moved collections project to Chap16_Generics and working with Stacks now

---
+++ Eclipse-Dev/Chap16_Generics/src/com/stacks/Stacks.java
@@ -0,0 +1,45 @@
+package com.stacks;
+
+import java.util.Stack;
+import java.util.EmptyStackException;
+import java.util.Iterator;
+
+public class Stacks {
+
+	public static void main(String[] args) {
+
+		Stack<Number> stack = new Stack<>(); // creates a stack
+		
+		// push elements to stack
+		stack.push(12L);
+		stack.push(5.55F);
+		stack.push(18.9D);
+		
+		//printStack(stack);
+		printStackEmpty(stack);
+		
+	}
+	
+	private static void printStackEmpty (Stack<Number> stack) {
+		try {
+			Number removedObject = null;
+			
+			while (true) {
+				removedObject = stack.pop();
+				System.out.printf("%s %s%n", "Popped element is", removedObject);
+			}
+		} catch (EmptyStackException ese) {
+			System.out.printf("Stack is empty");
+		
+		}
+	}
+	
+	// print stack using iterator
+	private static void printStack (Stack<Number> stack) {
+		Iterator<Number> stackIterator = stack.iterator();
+		
+		while (stackIterator.hasNext()) {
+			System.out.printf("%s%n", stackIterator.next());
+		}
+	}
+}
