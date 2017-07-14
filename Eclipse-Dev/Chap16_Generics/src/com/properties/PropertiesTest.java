f6b40252622616654e9eee936ecc1bd6504b1893
Radoslav Radoev <rradoev@gmail.com>
2017-07-14 11:11:28 -0700

    having fun with properties

--- Eclipse-Dev/Chap16_Generics/src/com/properties/PropertiesTest.java
+++ Eclipse-Dev/Chap16_Generics/src/com/properties/PropertiesTest.java
@@ -14,8 +14,67 @@ public class PropertiesTest {
 		table.setProperty("color", "blue");
 		table.setProperty("width", "200");
 		
-		System.out.println("After adding first two properties - color and width");
+		System.out.println(" After adding first two properties - color and width");
+		listProperties(table);
+		
+		// replace property value
+		table.setProperty("color", "red");
+		
+		System.out.println("\n After replacing propery color");
+		listProperties(table);
+		
+		saveProperties(table);
+		
+		System.out.println("\n After cleaning up the properties");
+		table.clear();
+		listProperties(table);
+		
+		// loading properties from the file
+		loadProperties(table);
+		
+		// get value of property color
+		Object value = table.getProperty("color");
+		
+		// check if value is in table
+		if (value != null) {
+			System.out.printf("\n Property color's value is %s%n", value);
+		}
+		else {
+			System.out.println("\n Property color not in table");
+		}
+	}
 	
+	private static void loadProperties(Properties props) {
+		try {
+			FileInputStream input = new FileInputStream("props.dat");
+			props.load(input);
+			input.close();
+			System.out.println("\n After loading properties");
+			listProperties(props);			
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+	}
+	
+	private static void saveProperties (Properties props) {
+		// save contents of table
+		try {
+			FileOutputStream output = new FileOutputStream("props.dat");
+			props.store(output, "Sample Properties");
+			output.close();
+			System.out.println("\n After saving properties to file");
+			listProperties(props);
+		} catch (IOException ioException) {
+			ioException.printStackTrace();
+		}
 	}
 
+	private static void listProperties (Properties props) {
+		Set <Object> keys = props.keySet();
+		
+		// output name/value pairs
+		for (Object key : keys) {
+			System.out.printf("%s\t%s%n", key, props.getProperty((String) key));
+		}
+	}
 }
