c42a6742f1ada730ec8eae536e4404bb8839f7ef
Radoslav Radoev <rradoev@gmail.com>
2017-07-15 19:01:46 -0700

    having fun chapter 16 exericeses.

---
+++ Eclipse-Dev/Chap16_Generics/src/com/ex1621/PirorityQueueTest.java
@@ -0,0 +1,28 @@
+package com.ex1621;
+
+import java.util.Collections;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+public class PirorityQueueTest {
+
+	public static void main(String[] args) {
+		// queue of capacity 11
+		Queue<Double> queue = new PriorityQueue<>(11, Collections.reverseOrder());
+		
+
+		// insert elements into queue
+		queue.offer(3.2);
+		queue.offer(9.8);
+		queue.offer(5.4);
+		
+		
+		// display elements in queue
+		while (queue.size() > 0) {
+			System.out.println(queue.poll());
+			
+		}
+
+	}
+
+}
