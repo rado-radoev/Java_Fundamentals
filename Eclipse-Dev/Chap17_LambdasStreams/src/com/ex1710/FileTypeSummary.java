1e4d10964e064d81efbbd1e6ce280875282f91cc
Radoslav Radoev <rradoev@gmail.com>
2017-07-23 22:00:21 -0700

    more lambdas - 17.10

--- Eclipse-Dev/Chap17_LambdasStreams/src/com/ex1710/FileTypeSummary.java
+++ Eclipse-Dev/Chap17_LambdasStreams/src/com/ex1710/FileTypeSummary.java
@@ -1,6 +1,6 @@
 package com.ex1710;
-
-import java.io.File;
+import java.util.Set;
+import java.util.TreeSet;
 import java.io.IOException;
 import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
@@ -9,12 +9,15 @@ import java.nio.file.Paths;
 import java.util.Scanner;
 import java.util.List;
 import java.util.ArrayList;
-import java.util.Arrays;
-
+import java.util.regex.Pattern;
+import java.util.Comparator;
+import java.util.stream.Collectors;
 
 public class FileTypeSummary {
 
 	public static void main(String[] args) throws IOException {
+		
+		Pattern pattern = Pattern.compile("\\.[A-Za-z]{3,}$");
 		
 		Scanner input = new Scanner(System.in);
 		
@@ -25,12 +28,17 @@ public class FileTypeSummary {
 		List<String> directories = new ArrayList<>();
 		dir.spliterator().forEachRemaining(curdir -> directories.add(curdir.toString()));
 		
-		directories.stream()
-			.map(entry -> Paths.get(entry))
-			.filter(entry -> Files.isDirectory((entry)));
+		Set<String> fileSummary = directories.stream()
+			.map(Paths::get)
+			.filter(Files::isRegularFile)
+			.flatMap(paths -> pattern.splitAsStream(paths.toString()))
+			.collect(Collectors.toCollection(TreeSet::new));
+
+		fileSummary.stream()
+			.sorted(Comparator.reverseOrder())
+			.forEach(System.out::println);
 
 		
-
 	}
 
 }
