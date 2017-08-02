26383c682a2710d452d7c4e25232ca3cf0422da0
superklamer <rradoev@gmail.com>
2017-08-01 20:21:09 -0700

    recursion exercices - I don't understand it

--- Eclipse-Dev/Chap18_Recursion/src/com/exBunnyEars/Count8.java
+++ Eclipse-Dev/Chap18_Recursion/src/com/exBunnyEars/Count8.java
@@ -12,8 +12,35 @@ package com.exBunnyEars;
 public class Count8 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int eight = 881888;
+		int count = 0;
+		int prev = 0;
+		while (eight > 0) {
+			int temp = eight % 10;			
+			if (temp == 8) {
+				if (prev == 8)
+					count+=2;
+				else 
+					count++;
+			}
+			eight /= 10;
+			prev = temp;
+		}
 
+		System.out.println(count);
+	}
+	
+	public static int count8(int n)
+	{
+		if(n == 0)
+			return 0;
+		if(n % 10 == 8)
+		{
+			if(n / 10 % 10 == 8)
+				return 2+count8(n/10);
+			return 1+count8(n/10);
+		}
+		return count8(n/10);
 	}
 
 }
