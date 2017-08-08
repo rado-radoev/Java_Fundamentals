313b0fe593016767d2f0d65dca57f3b78cbad3ac
superklamer <rradoev@gmail.com>
2017-08-08 07:29:39 -0700

    Insertion sort algo

---
+++ Eclipse-Dev/Chapter19_SearchingSorting/src/com/fig195/InsertionSort.java
@@ -0,0 +1,21 @@
+package com.fig195;
+
+public class InsertionSort {
+
+	public static void main(String[] args) {
+		int[] data = {34,56,4,10,77,51,93,30,5,52};
+	
+		for (int next = 1; next < data.length;next++) {
+			int insert = data[next];
+			int moveItem = next;
+			
+			while (moveItem > 0 && data[moveItem - 1] > insert) {
+				data[moveItem] = data[moveItem - 1];
+				moveItem--;
+			}
+			
+			data[moveItem] = insert;
+		}
+	}
+
+}
