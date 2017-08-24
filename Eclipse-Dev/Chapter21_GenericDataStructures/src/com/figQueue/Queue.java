60643a9e93150b803df3a341f30a1155a718df8c
superklamer <rradoev@gmail.com>
2017-08-23 22:26:33 -0700

    queues

---
+++ Eclipse-Dev/Chapter21_GenericDataStructures/src/com/figQueue/Queue.java
@@ -0,0 +1,33 @@
+package com.figQueue;
+import com.figLinkedList.List;
+import com.figLinkedList.EmptyListException;
+
+public class Queue<T> {
+	
+	private List<T> queueList;
+	
+	// constructor
+	public Queue() {
+		queueList = new List<T>("Queue");
+	}
+
+	// add object to queue
+	public void enqueue(T object) {
+		queueList.insertAtBack(object);
+	}
+	
+	// remove object from queue
+	public T dequeue() throws EmptyListException {
+		return queueList.removeFromFront();
+	}
+	
+	// detemine if queue is empty
+	public boolean isEmpty() {
+		return queueList.isEmpty();
+	}
+	
+	// output queue contents
+	public void print() {
+		queueList.print();
+	}
+}
