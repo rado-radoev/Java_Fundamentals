d8f8f17785ff6bc0af4bb25dcdcad2dcb50025f1
superklamer <rradoev@gmail.com>
2017-08-25 23:35:45 -0700

    ex 21.6

--- Eclipse-Dev/Chapter21_GenericDataStructures/src/com/ex216/ListConcatTest.java
+++ Eclipse-Dev/Chapter21_GenericDataStructures/src/com/ex216/ListConcatTest.java
@@ -8,16 +8,16 @@ public class ListConcatTest {
 		List<Integer> list2 = new List<>();
 		
 		// insert integers in list1
-		list1.insertAtFront(-1);
-		list1.insertAtFront(0);
-		list1.insertAtBack(1);
-		list1.insertAtBack(5);
+		list1.insertAtFront(1);
+		list1.insertAtFront(2);
+		list1.insertAtBack(3);
+		list1.insertAtBack(4);
 		
-		// insert integers in list1
-		list2.insertAtFront(-10);
-		list2.insertAtFront(1);
-		list2.insertAtBack(10);
-		list2.insertAtBack(50);
+		// insert integers in list2
+		list2.insertAtFront(5);
+		list2.insertAtFront(6);
+		list2.insertAtBack(7);
+		list2.insertAtBack(8);
 		
 		ListConcatenate.concatenate(list1, list2);
 		list1.print();
