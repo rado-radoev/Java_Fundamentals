bdb06dd19f1abc571e446d1cf6f3fe2ecae15d64
superklamer <rradoev@gmail.com>
2017-08-28 22:22:58 -0700

    the f*ing palindrome using stacks. could be better thoug. too many loops and if conditions.

--- Eclipse-Dev/Chapter21_GenericDataStructures/src/com/ex2111/Palindrome.java
+++ Eclipse-Dev/Chapter21_GenericDataStructures/src/com/ex2111/Palindrome.java
@@ -3,22 +3,45 @@ package com.ex2111;
 public class Palindrome {
 
 	public static void main(String[] args) {
-		String str = "Lay a wallaby baby ball away, Al";
+		String str = "alala";
 		
 		StackComposition<Character> stack = new StackComposition<Character>();
 		for (Character c : str.toCharArray()) {
+			if (isInRange((int)c, 48, 57) ||
+				isInRange((int)c, 65, 90) ||
+				isInRange((int)c, 97, 122))
 			stack.push(c);
 		}
-	
+		
+		boolean isPalindrome = false;
+		for (int i = 0; i < str.length(); i++) {
+			if (!stack.isEmpty()) {
+				if (isInRange((int)str.charAt(i), 48, 57) ||
+					isInRange((int)str.charAt(i), 65, 90) ||
+					isInRange((int)str.charAt(i), 97, 122)) {
+					
+					char current = stack.pop();
+					if (str.charAt(i) == current) {
+						isPalindrome = true;
+					}
+					else {
+						isPalindrome = false;
+					}
+				}
+			}
+		}
+		
+		if (isPalindrome) 
+			System.out.println("Is palindrome");
+		else
+			System.out.println("Is not palindrome");
 	}
 	
 	private static boolean isInRange(int num, int lowerRange, int higherRange) {
-		if (lowerRange > higherRange)
-			return false;
-		else if (higherRange < lowerRange)
-			return false;
-		else 
+		if (num >= lowerRange && num <= higherRange)
 			return true;
+		else
+			return false;
 	}
 
 }
