40b96133b377cf413583ab035523aad3817cf98e
superklamer <rradoev@gmail.com>
2017-09-11 22:21:35 -0700

    fig 22.5

---
+++ Eclipse-Dev/Chapter22_GUI_Part2/src/com/fig225/MenuTest.java
@@ -0,0 +1,13 @@
+package com.fig225;
+import javax.swing.JFrame;
+
+public class MenuTest {
+
+	public static void main(String[] args) {
+		MenuFrame menuFrame = new MenuFrame();
+		menuFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		menuFrame.setSize(500, 200);
+		menuFrame.setVisible(true);
+	}
+
+}
