0d0f6c07697865414990dab0811baf3264a022c9
Radoslav Radoev <rradoev@gmail.com>
2017-09-17 22:44:22 -0700

    ex 22.9 varying weightx and weighty

---
+++ Eclipse-Dev/Chapter22_GUI_Part2/src/com/ex229/Test.java
@@ -0,0 +1,17 @@
+package com.ex229;
+
+import javax.swing.JFrame;
+
+public class Test {
+
+	public static void main(String[] args) {
+		WeighXWeightY frame = new WeighXWeightY();
+		
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(300, 150);
+		frame.setVisible(true);
+	}
+
+}
+
+
