ed2e5291fe12ebb4589f9b673446eb39f50d4eee
Radoslav Radoev <rradoev@gmail.com>
2017-09-18 22:20:23 -0700

    ex22.11 wip -fields are updating as expected. TO DO: impelement the circle JPanel.

--- Eclipse-Dev/Chapter22_GUI_Part2/src/com/ex2211/MyColorChooser.java
+++ Eclipse-Dev/Chapter22_GUI_Part2/src/com/ex2211/MyColorChooser.java
@@ -8,12 +8,14 @@ import javax.swing.JSlider;
 import javax.swing.SwingConstants;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.TextField;
 
-public class MyColorChooser extends JFrame {
+public class MyColorChooser extends JFrame implements DocumentListener {
 
 	private final RGB[] colorNames = {RGB.RED, RGB.GREEN, RGB.BLUE};
 	private final JTextField[] colorTextFields;
@@ -21,15 +23,23 @@ public class MyColorChooser extends JFrame {
 	private final JSlider[] colorSliders;
 	private final JPanel displayValuesPanel;
 	private final JPanel displaySlidersPanel;
+	private final JPanel displayRectanglePanel;
+	private final RectanglePane rectangle;
 	
 	
 	public MyColorChooser() {
 		super("Color Chooser");
 		
+		// create rectangle pane
+		rectangle = new RectanglePane();
+		
 		// setup the two JPanels that will hold the labels and textfields and the sliders
 		displayValuesPanel = new JPanel();
 		displaySlidersPanel = new JPanel();
 		
+		// set up rectangle JPanel
+		displayRectanglePanel = new JPanel();
+		displayRectanglePanel.add(rectangle);
 		
 		// setup Labels
 		colorLabels = new JLabel[colorNames.length];
@@ -41,7 +51,9 @@ public class MyColorChooser extends JFrame {
 		// setup text fields
 		colorTextFields = new JTextField[colorNames.length];
 		for (int i = 0; i < colorTextFields.length; i++) {
-			colorTextFields[i] = new JTextField(3);
+			colorTextFields[i] = new JTextField("0", 3);
+			colorTextFields[i].setName(colorNames[i].name());
+			colorTextFields[i].getDocument().addDocumentListener(this);
 		}
 		
 		// add labels and text fields
@@ -65,6 +77,7 @@ public class MyColorChooser extends JFrame {
 		
 		add(displayValuesPanel, BorderLayout.NORTH);
 		add(displaySlidersPanel, BorderLayout.SOUTH);
+		add(displayRectanglePanel, BorderLayout.CENTER);
 	}
 	
 	private ChangeListener listener = new ChangeListener() {
@@ -73,9 +86,52 @@ public class MyColorChooser extends JFrame {
 		public void stateChanged(ChangeEvent e) {
 			for (int i = 0; i < colorSliders.length;i++) {
 				colorTextFields[i].setText(String.valueOf(colorSliders[i].getValue()));
+				
 			}	
 		}
 	};
+	
+	@Override
+	public void insertUpdate(DocumentEvent e) {
+		int r = 0, g = 0, b = 0;
+
+		for (JTextField textField : colorTextFields) {
+			if (textField.getName() == RGB.RED.name())
+				r = Integer.valueOf(textField.getText());
+			else if (textField.getName() == RGB.GREEN.name())
+				g = Integer.valueOf(textField.getText());
+			else if (textField.getName() == RGB.BLUE.name())
+				b = Integer.valueOf(textField.getText());
+		}
+		
+		Color c = new Color(r, g, b);
+		rectangle.setColor(c);
+		
+	}
+
+	@Override
+	public void removeUpdate(DocumentEvent e) {
+		int r = 0, g = 0, b = 0;
+
+		for (JTextField textField : colorTextFields) {
+			if (textField.getName() == RGB.RED.name())
+				r = Integer.valueOf(textField.getText());
+			else if (textField.getName() == RGB.GREEN.name())
+				g = Integer.valueOf(textField.getText());
+			else if (textField.getName() == RGB.BLUE.name())
+				b = Integer.valueOf(textField.getText());
+		}
+		
+		Color c = new Color(r, g, b);
+		rectangle.setColor(c);
+		
+	}
+
+	@Override
+	public void changedUpdate(DocumentEvent e) {
+
+		
+	}
 	
 }   
 
---
+++ Eclipse-Dev/Chapter22_GUI_Part2/src/com/ex2211/RectanglePane.java
@@ -0,0 +1,63 @@
+package com.ex2211;
+
+import javax.swing.JPanel;
+import java.awt.Graphics;
+import java.awt.Color;
+
+public class RectanglePane extends JPanel {
+
+	private Color color;
+	private int width;
+	private int height;
+	
+	public RectanglePane(int widht, int height, Color color) {
+		setWidth(widht);
+		setHeight(height);
+		this.color = color;
+	}
+	
+	public RectanglePane(int width, int height) {
+		setWidth(width);
+		setHeight(height);
+	}
+	
+	public RectanglePane() {
+		this(100, 100);
+	}
+	
+	@Override
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		
+		g.fillRect(5, 5, getWidth(), getHeight());
+	}
+	
+	
+	public int getWidth() {
+		return width;
+	}
+	
+	public int getHeight() {
+		return height;
+	}
+	
+	public Color getColor() {
+		return color;
+	}
+	
+	public void setHeight(int height) {
+		this.height = height;
+		repaint();
+	}
+	
+	public void setWidth(int width) {
+		this.width = width;
+		repaint();
+	}
+	
+	public void setColor(Color color) {
+		this.color = color;
+		repaint();
+	}
+	
+}
