0903907aa06fe90652e17bd161c2eba2a1d7b150
Radoslav Radoev <rradoev@gmail.com>
2017-09-22 22:27:52 -0700

    draw frame -wip

---
+++ Eclipse-Dev/Chapter22_GUI_Part2/src/com/ex2216_Test/Demo.java
@@ -0,0 +1,14 @@
+package com.ex2216_Test;
+
+import javax.swing.JFrame;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		DrawFrame drawFrame = new DrawFrame();
+		
+		drawFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		drawFrame.setSize(600, 700);
+		drawFrame.setVisible(true);
+	}
+}
--- Eclipse-Dev/Chapter22_GUI_Part2/src/com/ex2216_Test/DrawFrame.java
+++ Eclipse-Dev/Chapter22_GUI_Part2/src/com/ex2216_Test/DrawFrame.java
@@ -3,13 +3,16 @@ package com.ex2216_Test;
 import javax.swing.JFrame;
 import java.awt.BorderLayout;
 import javax.swing.JPanel;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JLabel;
 import javax.swing.JCheckBox;
 import java.awt.Color;
 import java.awt.event.ActionListener;
-import java.util.Iterator;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
 import java.awt.event.ActionEvent;
 
 public class DrawFrame extends JFrame {
@@ -43,8 +46,24 @@ public class DrawFrame extends JFrame {
 		statusBar = new JLabel();
 		
 		clearButton = new JButton("Clear");
+		clearButton.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
 		
 		undoButton = new JButton("Undo");
+		undoButton.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
 		
 		// fill colorNames
 		colorNames = new String[Colors.values().length];
@@ -53,7 +72,14 @@ public class DrawFrame extends JFrame {
 			colorNames[i] = col[i].getColorName();
 		}
 		colors = new JComboBox<String>(colorNames	);
-		
+		colors.addItemListener(new ItemListener() {
+			
+			@Override
+			public void itemStateChanged(ItemEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
 		
 		// fill shapeNames
 		shapeNames = new String[Shapes.values().length];
@@ -62,13 +88,40 @@ public class DrawFrame extends JFrame {
 			shapeNames[i] = sha[i].getShape();
 		}
 		shapes =  new JComboBox<String>(shapeNames);
+		shapes.addItemListener(new ItemListener() {
+			
+			@Override
+			public void itemStateChanged(ItemEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
 		
-		filled = new JCheckBox("Filled");		
+		filled = new JCheckBox("Filled");
+		filled.addChangeListener(new ChangeListener() {
+			
+			@Override
+			public void stateChanged(ChangeEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
 		
 		// setup the draw panel
 		drawPanel = new DrawPanel(statusBar);
 		
+		componentsPanel.add(undoButton);
+		componentsPanel.add(clearButton);
+		componentsPanel.add(colors);
+		componentsPanel.add(shapes);
+		componentsPanel.add(filled);
 		
+		drawingPanel.add(drawPanel);
 		
+		statusBarPanel.add(statusBar);
+		
+		add(componentsPanel, BorderLayout.NORTH);
+		add(drawingPanel, BorderLayout.CENTER);
+		add(statusBarPanel, BorderLayout.SOUTH);
 	}
 }
