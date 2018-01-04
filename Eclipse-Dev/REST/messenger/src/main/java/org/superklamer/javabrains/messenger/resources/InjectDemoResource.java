7a3565960cdc87099e62fb76e0fd4735f25cedbd
Radoslav Radoev <rradoev@gmail.com>
2018-01-04 00:49:33 -0800

    More on RESTful WebService

---
+++ Eclipse-Dev/REST/messenger/src/main/java/org/superklamer/javabrains/messenger/resources/InjectDemoResource.java
@@ -0,0 +1,27 @@
+package org.superklamer.javabrains.messenger.resources;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.CookieParam;
+import javax.ws.rs.GET;
+import javax.ws.rs.HeaderParam;
+import javax.ws.rs.MatrixParam;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/injectdemo")
+@Consumes(MediaType.TEXT_PLAIN)
+@Produces(MediaType.TEXT_PLAIN)
+public class InjectDemoResource {
+
+	@GET
+	@Path("annotations")
+	public String getParamsUsingAnotations(@MatrixParam("param") String matrixParam,
+										  @HeaderParam("customHeaderValue") String header,
+										  @CookieParam("name") String cookie) {
+		
+		return String.format("Matrix param is: %s%nCustom Header values is: %s%nCookie name: ", 
+				matrixParam, header, cookie);
+	}
+}
+ 
