f064492176a88467d7eefcd1a91ff4def6f2ebdc
Radoslav Radoev <rradoev@gmail.com>
2018-01-04 22:42:25 -0800

    More on RESTful WebService

--- Eclipse-Dev/REST/messenger/src/main/java/org/superklamer/javabrains/messenger/service/CommentService.java
+++ Eclipse-Dev/REST/messenger/src/main/java/org/superklamer/javabrains/messenger/service/CommentService.java
@@ -17,4 +17,30 @@ public class CommentService {
 		return new ArrayList<Comment>(comments.values());
 	}
 	
+	public Comment getComment(long messageId, long commentId) {
+		Map<Long, Comment> comments = messages.get(messageId).getComments();
+		return comments.get(commentId);
+	}
+	
+	public Comment addComment(long messageId, Comment comment) {
+		Map<Long, Comment> comments = messages.get(messageId).getComments();
+		comment.setId(comments.size() + 1);
+		comments.put(comment.getId(), comment);
+		return comment;
+	}
+	
+	public Comment updateComment(long messageId, Comment comment) {
+		Map<Long, Comment> comments = messages.get(messageId).getComments();
+		if (comment.getId() <= 0) 
+			return null;
+		
+		comments.put(comment.getId(), comment);
+		return comment;
+	}
+	
+	public Comment removeComment(long messageId, long commentId) {
+		Map<Long, Comment> comments = messages.get(messageId).getComments();
+		return comments.remove(commentId);
+	}
+	
 }
