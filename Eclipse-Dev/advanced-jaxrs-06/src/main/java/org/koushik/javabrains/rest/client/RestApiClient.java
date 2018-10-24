8c752119557b4d0992d5a3a7caba5263f490f311
Radoslav Radoev <rradoev@gmail.com>
2018-10-23 22:39:59 -0700

    post, invocations and generic types

--- Eclipse-Dev/advanced-jaxrs-06/src/main/java/org/koushik/javabrains/rest/client/RestApiClient.java
+++ Eclipse-Dev/advanced-jaxrs-06/src/main/java/org/koushik/javabrains/rest/client/RestApiClient.java
@@ -2,8 +2,12 @@ package org.koushik.javabrains.rest.client;
 
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.Response;
 
+import org.eclipse.persistence.oxm.MediaType;
+import org.eclipse.persistence.platform.database.PostgreSQLPlatform;
 import org.koushik.javabrains.messenger.model.Message;
 
 public class RestApiClient {
@@ -11,13 +15,39 @@ public class RestApiClient {
 	public static void main(String[] args) {
 		
 		Client client = ClientBuilder.newClient();
-
-		Response response = 
-				client.target("http://localhost:8080/advanced-jaxrs-06/webapi/messages/1")
+		
+		WebTarget baseTarget = client.target("http://localhost:8080/advanced-jaxrs-06/webapi/");
+		WebTarget messagesTarget = baseTarget.path("messages");
+		WebTarget singleMessageTarget = messagesTarget.path("{messageId}");
+		
+		Message message1 = singleMessageTarget
+					.resolveTemplate("messageId", "1")
 					.request()
-					.get();
-		Message message = response.readEntity(Message.class);
-		System.out.println(message.getMessage());
+					.get(Message.class);
+		
+		Message message2 = singleMessageTarget
+				.resolveTemplate("messageId", "2")
+				.request()
+				.get(Message.class);
+		
+		Message newMessage = new Message(4, "My new messge from JAX-RS client", "Rado");
+		Response postResponse = messagesTarget
+			.request()
+			.post(Entity.json(newMessage));
+
+		
+		
+		if (postResponse.getStatus() != 201) {
+			System.out.println("Error");
+		} else {
+			Message createdMessge = postResponse.readEntity(Message.class);
+			System.out.println(createdMessge.getMessage());
+		}
+		
+		
+//		Message message = response.readEntity(Message.class);
+//		System.out.println(message1.getMessage());
+//		System.out.println(message2.getMessage());
 	}
 
 }
