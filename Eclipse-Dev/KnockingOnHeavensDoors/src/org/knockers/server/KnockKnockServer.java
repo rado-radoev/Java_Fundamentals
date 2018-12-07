40224eed1c0f6060eeee09244d7a5a7ba59c5279
rradoev@gmail.com <rradoev@gmail.com>
2018-12-06 16:32:43 -0800

    Adding knock knock jokes

---
+++ Eclipse-Dev/KnockingOnHeavensDoors/src/org/knockers/server/KnockKnockServer.java
@@ -0,0 +1,31 @@
+package org.knockers.server;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+
+public class KnockKnockServer {
+	
+	private static int PORT;
+	private boolean listening = true;
+	
+	public KnockKnockServer(int port) throws IllegalArgumentException {
+		
+		if (port <= 0) throw new IllegalArgumentException("Port must be positive integer");
+		
+		this.PORT = port;
+		
+		
+		try (ServerSocket serverSocket = new ServerSocket(port)) {
+				while (listening) {
+					new KnockKnockMultiServerThread(serverSocket.accept()).start();
+				}
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.err.println(String.format("Could not listen on port %d", port));
+			System.exit(-1);
+		}
+		
+		
+		
+	}
+}
